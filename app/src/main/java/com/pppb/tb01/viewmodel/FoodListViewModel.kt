0789285a8ac05b42e039e54e29bb24c16666dae2
frostygum <4nthonius.juan5@gmail.com>
2020-10-18 21:18:58 +0700

    remove dummy data

--- app/src/main/java/com/pppb/tb01/viewmodel/FoodListViewModel.kt
+++ app/src/main/java/com/pppb/tb01/viewmodel/FoodListViewModel.kt
@@ -9,14 +9,6 @@ class FoodListViewModel(): ViewModel() {
     private val foodList: MutableList<Food> = mutableListOf()
     private val foods: MutableLiveData<List<Food>> = MutableLiveData<List<Food>>()
 
-    init {
-        for(i in 1 .. 10) {
-            val food = Food("nama food ke $i", "ini description untuk food ke $i")
-            addFood(food)
-        }
-        this.update()
-    }
-
     fun getFoods() = foods as LiveData<List<Food>>
 
     fun addFood(food: Food) {
