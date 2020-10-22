0861e6bba205f50ef263fb683dd1461a84f1320e
frostygum <4nthonius.juan5@gmail.com>
2020-10-22 16:03:22 +0700

    Implementing new changePage method

--- app/src/main/java/com/pppb/tb01/viewmodel/PageViewModel.kt
+++ app/src/main/java/com/pppb/tb01/viewmodel/PageViewModel.kt
@@ -5,16 +5,16 @@ import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 class PageViewModel() : ViewModel() {
-    private val page: MutableLiveData<Pair<Int, Boolean>> = MutableLiveData()
+    private val page: MutableLiveData<Pair<String, Boolean>> = MutableLiveData()
     private val leftDrawerState: MutableLiveData<Boolean> = MutableLiveData()
     private val selectedFoodId: MutableLiveData<Int> = MutableLiveData()
     private val title: MutableLiveData<String> = MutableLiveData()
 
     init {
-        changePage(1)
+        changePage("HOME")
     }
 
-    fun getPage() = this.page as LiveData<Pair<Int, Boolean>>
+    fun getPage() = this.page as LiveData<Pair<String, Boolean>>
 
     fun getLeftDrawerState() = this.leftDrawerState as LiveData<Boolean>
 
@@ -22,8 +22,8 @@ class PageViewModel() : ViewModel() {
 
     fun getTitle() = this.title as MutableLiveData<String>
 
-    fun changePage(pageNumber: Int, popBackStack: Boolean = false) {
-        this.page.value = Pair(pageNumber, popBackStack)
+    fun changePage(pageName: String, popBackStack: Boolean = false) {
+        this.page.value = Pair(pageName, popBackStack)
     }
 
     fun closeLeftDrawer() {
